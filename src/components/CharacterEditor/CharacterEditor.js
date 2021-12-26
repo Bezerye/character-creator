c17456172e2496e937ce6acfdb060d9963d00d22
Erkan <Bezerye@gmail.com>
2021-12-27 01:21:31 +0300

    solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -26,10 +26,11 @@ function App() {
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
   const [clothesColor, setClothesColor] = React.useState(
     defaultClothesColor
-  );
+    );
 
-  return (
-    <main className={styles.characterEditor}>
+    return (
+      <main className={styles.characterEditor}>
+        <div className={styles.grayBg}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -76,8 +77,6 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
-      </MaxWidthWrapper>
-
       <div className={styles.characterWrapper}>
         <Character
           body={body}
@@ -88,6 +87,7 @@ function App() {
           clothesColor={clothesColor}
         />
       </div>
+      </MaxWidthWrapper>
     </main>
   );
 }
